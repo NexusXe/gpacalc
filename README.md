74ee819d38279ba7bd2a159f2c006eab49188dc5
Briar Campbell <andastrike@gmail.com>
2020-09-30 02:39:15 +0000

    better

--- README.md
+++ README.md
@@ -5,5 +5,18 @@ Python library for calculating GPA.
 You can import the calculator like so:
 ```py 
 from gpa import calc
+
+
 ```
-`calc` takes a `dict` type as an input, in a structure like so:
+`calc` takes a `dict` type as an input, in a structure like so, where the first value is your pertcentage in your class out of 100.0 and the second is the amount of credit-hours that the class is worth:
+```py
+scores = {
+    "Math": [80.1, 2],
+    "Chemistry": [92.4, 2.0],
+    "History": [68.1, 3.0],
+    "English": [32.0, 3]
+}
+```
+
+For the first value, entering an `int` will get it converted to a `float` and divided by 100. For example, `15` would be interpreted as `15.0`, or 15 percent.
+For the second value, an `int`, `str`, or `float` type is accepted.
--- main.py
+++ main.py
@@ -1,1 +1,39 @@
-from gpa import calc
+def main(scores):
+
+    try:
+        for x in scores:
+            class_name = x
+            weight = scores[class_name]
+            class_percentage = float(weight[0])
+            print('Class Name: ' + str(class_name))
+            print('Class Percentage: ' + str(class_percentage))
+            if len(weight) > 1:
+                class_credits = float(weight[1])
+                print('Class Credits: ' + str(class_credits))
+            if len(weight) > 2:
+                class_multiplier = float(weight[2])
+                print('Class Multiplier: ' + str(class_multiplier))
+    except ValueError:
+        print('Unable to parse one of the recieved values. Make sure that all of the values are numbers.')
+
+
+    def crunch(class_percentage, class_credits=2, class_multiplier=1):
+        if class_percentage >= 93:
+            grade_points = 4
+        elif class_percentage >=92 and class_percent
+        
+
+
+
+
+scores = {
+    "Math": [80.1, 2],
+    "Chemistry": [92.4, 2],
+    "History": [68.1, 3],
+    "English": [31, 3, 1.5]
+}
+
+
+if __name__ == '__main__':
+    main(scores)
+    
